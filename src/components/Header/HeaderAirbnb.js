4c0d3d057c79c744bba34932e81329e2e995edf5
windsome <36081189@qq.com>
2016-08-26 00:09:00 +0800

    add HeaderAirbnb

---
+++ src/components/Header/HeaderAirbnb.js
@@ -0,0 +1,79 @@
+import React from 'react'
+import { IndexLink, Link } from 'react-router'
+import classes from './HeaderAirbnb.scss'
+import classNames from 'classnames';
+
+const Logo = () => (
+  <div>
+    <span className={classNames(classes.icon, "glyphicon", "glyphicon-home")} aria-hidden="true"></span>
+    {/* 'Home' */}
+  </div>
+)
+
+const Tour = () => (
+  <div>
+    { 'Tour' }
+    <span className={classNames(classes.icon, "glyphicon", "glyphicon-sound-stereo")} aria-hidden="true"></span>
+  </div>
+)
+
+const Message = () => (
+  <div>
+    { 'Tour' }
+    {/*<span className="glyphicon glyphicon-envelope" aria-hidden="true"></span>*/}
+    <i className={classNames(classes.icon, "glyphicon", "glyphicon-envelope")}>
+      <i className={classNames(classes.numberBadge, "badge")}>1</i>
+    </i>
+  </div>
+)
+
+const Help = () => (
+  <div>
+    <span>Help</span>
+    <span className={classNames(classes.icon, "glyphicon", "glyphicon-question-sign")} aria-hidden="true"></span>
+  </div>
+)
+
+const User = (props) => (
+  <div>
+    <span>{props.userName || 'Login'}</span>
+    <span className={classNames(classes.icon, "glyphicon")}><img src={props.avatar || './favicon.ico'} alt="..." className={classNames(classes.avatar, "img-circle")} /></span>
+  </div>
+)
+
+export const Header = (props) => (
+  <div className={classes.airbnbHeader}>
+    <div className={classNames(classes.comp, "pull-left")}>
+    <IndexLink to='/' activeClassName={classes.activeRoute}>
+      <Logo />
+    </IndexLink>
+    </div>
+
+    <div className={classNames(classes.comp, "pull-right")}>
+    <Link to='/zen' activeClassName={classes.activeRoute}>
+      <User />
+    </Link>
+    </div>
+
+    <div className={classNames(classes.comp, "pull-right")}>
+    <Link to='/counter' activeClassName={classes.activeRoute}>
+      <Help />
+    </Link>
+    </div>
+
+    <div className={classNames(classes.comp, "pull-right")}>
+    <Link to='/counter' activeClassName={classes.activeRoute}>
+      <Message />
+    </Link>
+    </div>
+
+    <div className={classNames(classes.comp, "pull-right")}>
+    <Link to='/counter' activeClassName={classes.activeRoute}>
+      <Tour />
+    </Link>
+    </div>
+
+  </div>
+)
+
+export default Header
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,3 +1,3 @@
-import Header from './Header'
+import Header from './HeaderAirbnb'
 
 export default Header
